28b662afa1322f1b58992f63e691dc22313884bb
antar <denis_penkin@mail.ru>
2026-03-28 16:55:11 +0300

    Enable USB logging for static LED strip debugging

--- src/static_led_strip.c
+++ src/static_led_strip.c
@@ -3,7 +3,10 @@
 #include <zephyr/drivers/led_strip.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
+
+LOG_MODULE_REGISTER(yolochka_static_led_strip, CONFIG_LOG_DEFAULT_LEVEL);
 
 #if DT_HAS_CHOSEN(yolochka_static_led_strip)
 
@@ -22,12 +25,18 @@
 #define AUX_LED_BLUE 0
 
 static struct k_work_delayable aux_led_start_work;
+static uint32_t aux_led_attempt;
 
 static void yolochka_static_led_strip_start(struct k_work *work) {
     const struct device *strip = DEVICE_DT_GET(AUX_LED_STRIP_NODE);
     struct led_rgb pixels[AUX_LED_COUNT];
+    int rc;
+
+    ARG_UNUSED(work);
+    aux_led_attempt++;
 
     if (!device_is_ready(strip)) {
+        LOG_ERR("Attempt %u: strip device %s is not ready", aux_led_attempt, strip->name);
         return;
     }
 
@@ -37,13 +46,22 @@ static void yolochka_static_led_strip_start(struct k_work *work) {
         pixels[i].b = AUX_LED_BLUE;
     }
 
-    (void)led_strip_update_rgb(strip, pixels, ARRAY_SIZE(pixels));
+    rc = led_strip_update_rgb(strip, pixels, ARRAY_SIZE(pixels));
+    if (rc < 0) {
+        LOG_ERR("Attempt %u: led_strip_update_rgb(%s) failed: %d", aux_led_attempt, strip->name,
+                rc);
+    } else {
+        LOG_INF("Attempt %u: updated %u pixels on %s", aux_led_attempt,
+                (unsigned int)ARRAY_SIZE(pixels), strip->name);
+    }
+
     k_work_schedule(&aux_led_start_work, K_MSEC(AUX_LED_REFRESH_MS));
 }
 
 static int yolochka_static_led_strip_init(void) {
     k_work_init_delayable(&aux_led_start_work, yolochka_static_led_strip_start);
     k_work_schedule(&aux_led_start_work, K_MSEC(AUX_LED_STARTUP_DELAY_MS));
+    LOG_INF("Scheduled static LED strip updates for %s", DEVICE_DT_NAME(AUX_LED_STRIP_NODE));
 
     return 0;
 }
